846126ddef66ccb2cd4770d185e4739420685286
İbrahim Halil Bayat <61160630+baggins34@users.noreply.github.com>
2023-02-16 09:02:30 +0300

    Add files via upload

---
+++ take_images_for_calibration.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+
+
+CamL= cv2.VideoCapture(0) # Left camera
+CamR= cv2.VideoCapture(1) # Right camera
+
+def main():
+
+    image_id = 0
+
+    while True:
+        if not (CamL.grab() and CamR.grab()):
+            print("No more frames")
+            break
+
+        _, left_frame = CamL.retrieve()
+        _, right_frame = CamR.retrieve()
+
+        cv2.imshow('capL', left_frame)
+        cv2.imshow('capR', right_frame)
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+        elif key == ord('c'):
+            cv2.imwrite("office_images/left" + str(image_id) + ".png", left_frame)
+            cv2.imwrite("office_images/right" + str(image_id) + ".png", right_frame)
+            i += 1
+
+    CamL.release()
+    CamR.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
